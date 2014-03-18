a8625e985246a74d385967019a5b96eb9ab86ba6
Cy <whatever>
2014-03-17 19:12:48 -0700

    Better dependency tracking
    It's still hecka hacky

--- compile.lua
+++ compile.lua
@@ -5,28 +5,36 @@ local myexecute = function(command)
     return os.execute(command)
 end
 
+function needUpdate(target,source)
+    tattr = lfs.attributes(target,'modification')
+    if not tattr then return true end
+    sattr = lfs.attributes(source,'modification')
+    if not sattr then error("Required source "..source.." does not exist!") end
+    if sattr > tattr then return true end
+    return false
+end
+
 local function compilerequire(name, options)
     dest = name .. ".so"
-    attr = lfs.attributes(dest,'access')
-    if attr then 
-        return require(name)
+
+    source = (options and options.source) or name .. ".c"
+    object = name..".o"
+
+    if needUpdate(dest,object) or needUpdate(dest,source) then
+        local cargs = ''
+        local largs = ''
+        if options then
+            if options.cargs then
+                cargs = options.cargs
+            end
+            if options.largs then
+                largs = options.largs
+            end
+        end
+        assert(0==myexecute("gcc -O2 -fPIC -I/usr/include/luajit-2.0 -c "..source.." -o "..object.." "..cargs))
+        assert(0==myexecute("gcc -shared -o "..dest.." "..name..".o "..largs))
     end
 
-    local cargs = ''
-    local largs = ''
-    if options then
-        if options.cargs then
-            cargs = options.cargs
-        end
-        if options.largs then
-            largs = options.largs
-        end
-    end
-    source = options.source or name .. ".c"
-    
-    assert(0==myexecute("gcc -O2 -fPIC -I/usr/include/luajit-2.0 -c "..source.." -o "..name..".o "..cargs))
-    assert(0==myexecute("gcc -shared -o "..dest.." "..name..".o "..largs))
-    
     return require(name)
 end
 
