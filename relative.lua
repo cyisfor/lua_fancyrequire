78921bba2fd905f748148407a16ebaa057ee850b
user <nobody>
2014-11-10 03:09:41 -0800

    sigh

--- relative.lua
+++ relative.lua
@@ -1,10 +1,20 @@
 local path = require('pl.path')
+local posix = require('posix')
 
 table.insert(package.loaders,1,function(name)
     -- parent...require...thisfunction
     local parent = debug.getinfo(3,'S')
     if not parent then return "No relative imports from non-files" end
-    local location = path.join(path.dirname(parent.short_src),name)
+    parent = parent.short_src
+    while true do
+        local link = posix.readlink(parent)
+        if not link then break end
+        parent = path.relpath(path.abspath(link,path.dirname(parent)))
+    end
+    local location = path.dirname(parent)
+    for sub in name:gmatch('[^%.]+') do
+        location = path.join(location,sub)
+    end
     local src = location..'.lua'
     if path.isfile(src) then
         -- print('found relative source',src)
